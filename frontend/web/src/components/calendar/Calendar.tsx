d29e5556eda7fb670e34ee9ecd26575e8612db63
Victor Gomez <39098525+Victin09@users.noreply.github.com>
2022-06-29 16:12:45 +0000

    feature: working on calendar styles

--- frontend/web/src/components/calendar/Calendar.tsx
+++ frontend/web/src/components/calendar/Calendar.tsx
@@ -1,6 +1,6 @@
-import { Calendar, momentLocalizer, HeaderProps, ToolbarProps } from "react-big-calendar";
+import { Calendar, momentLocalizer, HeaderProps, ToolbarProps, EventProps, EventPropGetter, EventWrapperProps } from "react-big-calendar";
 import moment from "moment";
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import BoxWrapper from "../shared/BoxWrapper";
 import { useQuery } from "react-query";
 import { getEvents } from "../../api/event";
@@ -8,33 +8,33 @@ import { useSelectedGuild } from "../../store/useSelectedGuild";
 import { useDisclosure } from "@chakra-ui/hooks";
 import CreateEventModal from "./CreateEventModal";
 import 'moment/locale/es';
+import './calendar.scss'
+import useOnClickOutside from "../../hooks/useClickOutside";
 
 const localizer = momentLocalizer(moment);
 
 const HeaderCellContent: React.FC<HeaderProps> = ({ label }) => {
     return (
-        <span className="font-semibold">
-            {label.toUpperCase()}
-        </span>
+        <div>
+            <div className="bg-base-200">
+                <span className="font-bold">
+                    {label.toUpperCase()}
+                </span>
+            </div>
+        </div>
     );
 };
 
 const ToolbarContent: React.FC<ToolbarProps> = (toolbar) => {
-    console.log({ toolbar })
     const goToBack = () => {
-        toolbar.date.setMonth(toolbar.date.getMonth() - 1);
         toolbar.onNavigate('PREV');
     };
 
     const goToNext = () => {
-        toolbar.date.setMonth(toolbar.date.getMonth() + 1);
         toolbar.onNavigate('NEXT');
     };
 
     const goToCurrent = () => {
-        const now = new Date();
-        toolbar.date.setMonth(now.getMonth());
-        // toolbar.date.setYear(now.getFullYear());
         toolbar.onNavigate('TODAY');
     };
 
@@ -49,22 +49,56 @@ const ToolbarContent: React.FC<ToolbarProps> = (toolbar) => {
         <div className="flex justify-between items-center">
 
             <div className="flex m-1">
-                <button className="btn btn-primary" onClick={goToBack}>&#8249;</button>
-                <button className="btn btn-primary" onClick={goToCurrent}>Hoy</button>
-                <button className="btn btn-primary" onClick={goToNext}>&#8250;</button>
+                <button className="btn btn-sm btn-primary m-1" onClick={goToBack}>&#8249;</button>
+                <button className="btn btn-sm btn-primary m-1" onClick={goToCurrent}>Hoy</button>
+                <button className="btn btn-sm btn-primary m-1" onClick={goToNext}>&#8250;</button>
             </div>
             <label className="font-bold">{toolbar.label.toUpperCase()}</label>
 
-            <div className="flex m-1">
-                <button className="btn btn-primary" onClick={() => toolbar.onView("month")}>Mes</button>
-                <button className="btn btn-primary" onClick={() => toolbar.onView("week")}>Semana</button>
-                <button className="btn btn-primary" onClick={() => toolbar.onView("day")}>Día</button>
-                <button className="btn btn-primary" onClick={() => toolbar.onView("agenda")}>Agenda</button>
+            <div className="flex">
+                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("month")}>Mes</button>
+                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("week")}>Semana</button>
+                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("day")}>Día</button>
+                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("agenda")}>Agenda</button>
             </div>
         </div >
     );
 };
 
+// const EventWrapperContent: React.FC<EventWrapperProps> = ({ event, onClick, accessors, className, continuesEarlier, continuesLater, getters, isRtl, label, onDoubleClick, selected, children }) => {
+//     console.log('event', event)
+//     console.log('selected', selected)
+//     // console.log('title', title)
+
+//     const ref = useRef<HTMLDivElement>(null)
+//     const [eventSelected, setEventSelected] = useState<boolean>(selected)
+
+//     const handleClickOutside = () => {
+//         setEventSelected(false)
+//     }
+
+//     useOnClickOutside(ref, handleClickOutside)
+
+//     return (
+//         <div ref={ref} className={` p-1 rounded text-primary-content w-full`} onClick={() => setEventSelected(!eventSelected)}>
+//             {/* {eventSelected && (<span className="badge">Elected</span>)}
+//             <span className="font-bold">{event.title}</span> */}
+//             <div >
+//                 {children}
+//             </div>
+//         </div >
+//     )
+// }
+
+// const EventContent: React.FC<EventProps> = ({ event, title }) => {
+//     console.log('title', title)
+//     console.log('eventEvent', event)
+//     return (
+//         <div className="">
+//             <div>{event.title}</div>
+//         </div>
+//     )
+// }
 
 const MyCalendar: React.FC = () => {
     const disclosure = useDisclosure();
@@ -77,14 +111,33 @@ const MyCalendar: React.FC = () => {
     );
     const [modalData, setModalData] = React.useState<Date | null>(null);
 
+    const eventPropGetter = useCallback(
+        (event, start, end, isSelected) => (
+            {
+                ...({
+                    className: 'bg-primary'
+                }),
+                ...(moment(start).hour() <= 1 && {
+                    className: 'bg-warning',
+                }),
+                ...(moment(end).day() < new Date().getDay() && {
+                    className: 'bg-success',
+                }),
+                ...(isSelected && {
+                    // set bg-info to important
+                    className: 'text-primary bg-info',
+                }),
+            }),
+        []
+    )
+
+
     if (isLoading) return null;
 
     const handleDateClick = (currentDate: Date) => {
         setModalData(currentDate);
         onOpen();
     };
-
-    console.log({ data })
 
     return (
         <>
@@ -104,9 +157,16 @@ const MyCalendar: React.FC = () => {
                     startAccessor="start"
                     endAccessor="end"
                     style={{ height: "100%", width: "100%", color: "#1D1C1B" }}
+                    eventPropGetter={eventPropGetter}
                     components={{
                         header: HeaderCellContent,
-                        toolbar: ToolbarContent
+                        toolbar: ToolbarContent,
+                        // event: EventContent,
+                        // eventWrapper: ({ event, onClick, accessors, className, continuesEarlier, continuesLater, getters, isRtl, label, onDoubleClick, selected, children }) => (
+                        //     <EventWrapperContent event={event} onClick={onClick} accessors={accessors} className={className}
+                        //         continuesEarlier={continuesEarlier} continuesLater={continuesLater} getters={getters}
+                        //         isRtl={isRtl} label={label} onDoubleClick={onDoubleClick} selected={selected}>{children}</EventWrapperContent>
+                        // ),
                     }}
                 />
             </BoxWrapper>
