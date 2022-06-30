6bedad6da7a9ef75615681a103c121afbdb2481b
Victor Gomez <39098525+Victin09@users.noreply.github.com>
2022-06-30 10:58:12 +0000

    feature: add calendar events modal

--- frontend/web/src/components/calendar/Calendar.tsx
+++ frontend/web/src/components/calendar/Calendar.tsx
@@ -14,55 +14,55 @@ import useOnClickOutside from "../../hooks/useClickOutside";
 const localizer = momentLocalizer(moment);
 
 const HeaderCellContent: React.FC<HeaderProps> = ({ label }) => {
-    return (
-        <div>
-            <div className="bg-base-200">
-                <span className="font-bold">
-                    {label.toUpperCase()}
-                </span>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="bg-base-200">
+        <span className="font-bold">
+          {label.toUpperCase()}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 const ToolbarContent: React.FC<ToolbarProps> = (toolbar) => {
-    const goToBack = () => {
-        toolbar.onNavigate('PREV');
-    };
+  const goToBack = () => {
+    toolbar.onNavigate('PREV');
+  };
 
-    const goToNext = () => {
-        toolbar.onNavigate('NEXT');
-    };
+  const goToNext = () => {
+    toolbar.onNavigate('NEXT');
+  };
 
-    const goToCurrent = () => {
-        toolbar.onNavigate('TODAY');
-    };
+  const goToCurrent = () => {
+    toolbar.onNavigate('TODAY');
+  };
 
-    const label = () => {
-        const date = moment(toolbar.date);
-        return (
-            <span><b>{date.format('MMMM')}</b><span> {date.format('YYYY')}</span></span>
-        );
-    };
-
+  const label = () => {
+    const date = moment(toolbar.date);
     return (
-        <div className="flex justify-between items-center">
-
-            <div className="flex m-1">
-                <button className="btn btn-sm btn-primary m-1" onClick={goToBack}>&#8249;</button>
-                <button className="btn btn-sm btn-primary m-1" onClick={goToCurrent}>Hoy</button>
-                <button className="btn btn-sm btn-primary m-1" onClick={goToNext}>&#8250;</button>
-            </div>
-            <label className="font-bold">{toolbar.label.toUpperCase()}</label>
-
-            <div className="flex">
-                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("month")}>Mes</button>
-                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("week")}>Semana</button>
-                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("day")}>Día</button>
-                <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("agenda")}>Agenda</button>
-            </div>
-        </div >
+      <span><b>{date.format('MMMM')}</b><span> {date.format('YYYY')}</span></span>
     );
+  };
+
+  return (
+    <div className="flex justify-between items-center">
+
+      <div className="flex m-1">
+        <button className="btn btn-sm btn-primary m-1" onClick={goToBack}>&#8249;</button>
+        <button className="btn btn-sm btn-primary m-1" onClick={goToCurrent}>Hoy</button>
+        <button className="btn btn-sm btn-primary m-1" onClick={goToNext}>&#8250;</button>
+      </div>
+      <label className="font-bold">{toolbar.label.toUpperCase()}</label>
+
+      <div className="flex">
+        <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("month")}>Mes</button>
+        <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("week")}>Semana</button>
+        <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("day")}>Día</button>
+        <button className="btn btn-sm btn-primary m-1" onClick={() => toolbar.onView("agenda")}>Agenda</button>
+      </div>
+    </div >
+  );
 };
 
 // const EventWrapperContent: React.FC<EventWrapperProps> = ({ event, onClick, accessors, className, continuesEarlier, continuesLater, getters, isRtl, label, onDoubleClick, selected, children }) => {
@@ -101,82 +101,97 @@ const ToolbarContent: React.FC<ToolbarProps> = (toolbar) => {
 // }
 
 const MyCalendar: React.FC = () => {
-    const disclosure = useDisclosure();
-    const { onOpen } = disclosure;
-    const selectedGuild = useSelectedGuild((state) => state.selectedGuild);
-    const { data, isLoading } = useQuery(
-        "events",
-        () => getEvents(selectedGuild?.id),
-        { enabled: !!selectedGuild }
-    );
-    const [modalData, setModalData] = React.useState<Date | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const selectedGuild = useSelectedGuild((state) => state.selectedGuild);
+  const { data, isLoading } = useQuery(
+    "events",
+    () => getEvents(selectedGuild?.id),
+    { enabled: !!selectedGuild }
+  );
+  const [modalData, setModalData] = React.useState<Date | null>(null);
+
+  // const DateHeaderContent: React.FC<any> = (props) => {
+  //   const handleCick = () => {
+  //     setModalData(props.date)
+  //   }
+
+  //   return (
+  //     <div>
+  //       <label htmlFor="createEventModal" className="hover:cursor-pointer">{props.label}</label>
+  //     </div>
+  //   )
+  // }
+
+  /**
+   * TODO: Fix dates
+   */
+  const eventPropGetter = useCallback(
+    (event, start, end, isSelected) => (
+      {
+        ...({
+          className: 'bg-primary font-semibold rounded'
+        }),
+        ...(moment(start).isAfter(moment().subtract(1, 'hours')) && {
+          className: 'bg-warning font-semibold rounded',
+        }),
+        ...(moment(end).isBefore(moment()) && {
+          className: 'bg-success font-semibold rounded',
+        }),
+        ...(isSelected && {
+          className: 'bg-info font-semibold outline-0 rounded',
+        }),
+      }),
+    []
+  )
 
 
-    /**
-    * TODO: Fix dates
-    */
-    const eventPropGetter = useCallback(
-        (event, start, end, isSelected) => (
-            {
-                ...({
-                    className: 'bg-primary rounded'
-                }),
-                ...(moment(start).hour() <= 1 && {
-                    className: 'bg-warning',
-                }),
-                ...(moment(end).day() < new Date().getDay() && {
-                    className: 'bg-success',
-                }),
-                ...(isSelected && {
-                    // set bg-info to important
-                    className: 'bg-info outline-1',
-                }),
-            }),
-        []
-    )
+  if (isLoading) return null;
 
+  const handleDateClick = (currentDate: Date) => {
+    setModalData(currentDate);
+    setShowModal(true)
+    console.log('currentDateHandleClick', currentDate)
+  };
 
-    if (isLoading) return null;
+  return (
+    <>
+      <BoxWrapper>
+        <Calendar
+          onDrillDown={handleDateClick}
+          localizer={localizer}
+          events={data.data}
+          views={["month", "week", "day", "agenda"]}
+          messages={{
+            next: "Siguiente", previous: "Atrás", today: "Hoy", month: "Mes", week: "Semana", day: "Día", agenda: "Agenda", date: 'Fecha',
+            time: 'Tiempo',
+            event: 'Evento',
+            allDay: "Todo el día",
 
-    const handleDateClick = (currentDate: Date) => {
-        setModalData(currentDate);
-        onOpen();
-    };
-
-    return (
-        <>
-            <BoxWrapper>
-                <Calendar
-                    onDrillDown={handleDateClick}
-                    localizer={localizer}
-                    events={data.data}
-                    views={["month", "week", "day", "agenda"]}
-                    messages={{
-                        next: "Siguiente", previous: "Atrás", today: "Hoy", month: "Mes", week: "Semana", day: "Día", agenda: "Agenda", date: 'Fecha',
-                        time: 'Tiempo',
-                        event: 'Evento',
-                        allDay: "Todo el día",
-
-                    }}
-                    startAccessor="start"
-                    endAccessor="end"
-                    style={{ height: "100%", width: "100%", color: "#1D1C1B" }}
-                    eventPropGetter={eventPropGetter}
-                    components={{
-                        header: HeaderCellContent,
-                        toolbar: ToolbarContent,
-                        // event: EventContent,
-                        // eventWrapper: ({ event, onClick, accessors, className, continuesEarlier, continuesLater, getters, isRtl, label, onDoubleClick, selected, children }) => (
-                        //     <EventWrapperContent event={event} onClick={onClick} accessors={accessors} className={className}
-                        //         continuesEarlier={continuesEarlier} continuesLater={continuesLater} getters={getters}
-                        //         isRtl={isRtl} label={label} onDoubleClick={onDoubleClick} selected={selected}>{children}</EventWrapperContent>
-                        // ),
-                    }}
-                />
-            </BoxWrapper>
-            <CreateEventModal disclosure={disclosure} currentDate={modalData!} />
-        </>
-    );
+          }}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: "100%", width: "100%", color: "#1D1C1B" }}
+          eventPropGetter={eventPropGetter}
+          components={{
+            header: HeaderCellContent,
+            toolbar: ToolbarContent,
+            // event: EventContent,
+            // eventWrapper: ({ event, onClick, accessors, className, continuesEarlier, continuesLater, getters, isRtl, label, onDoubleClick, selected, children }) => (
+            //     <EventWrapperContent event={event} onClick={onClick} accessors={accessors} className={className}
+            //         continuesEarlier={continuesEarlier} continuesLater={continuesLater} getters={getters}
+            //         isRtl={isRtl} label={label} onDoubleClick={onDoubleClick} selected={selected}>{children}</EventWrapperContent>
+            // ),
+            // month: {
+            //   dateHeader: DateHeaderContent
+            // }
+          }}
+        />
+      </BoxWrapper>
+      {showModal && (
+        <CreateEventModal currentDate={modalData!} handleClick={() => setShowModal(false)} />
+      )}
+    </>
+  );
 };
 
 export default MyCalendar;
--- frontend/web/src/components/calendar/CreateEventModal.tsx
+++ frontend/web/src/components/calendar/CreateEventModal.tsx
@@ -1,20 +1,7 @@
-import { UseDisclosureProps } from "@chakra-ui/hooks";
-import { Modal } from "@chakra-ui/modal";
-import {
-    Button,
-    FormControl,
-    FormLabel,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalHeader,
-    ModalOverlay,
-    VStack,
-} from "@chakra-ui/react";
 import { EventAttributes } from "@tidify/common";
 import { Formik, Form } from "formik";
 import moment from "moment";
-import React from "react";
+import React, { forwardRef, useState } from "react";
 import { useQueryClient, useMutation } from "react-query";
 import { createEvent } from "../../api/event";
 import { Response } from "../../types";
@@ -22,151 +9,110 @@ import FormInput from "../auth/FormInput";
 import DatePicker from "../../ui/DatePicker";
 import { useSelectedGuild } from "../../store/useSelectedGuild";
 
+import "moment/locale/es"
+
 interface Props {
-    disclosure: UseDisclosureProps;
-    currentDate: Date;
+  currentDate: Date;
+  handleClick: () => void
 }
 
+const CustomInput = forwardRef<HTMLInputElement>(({ value, onClick }: any, ref) => (
+  <div className="form-control w-full max-w-xs">
+    <label className="label">
+      <span className="label-text">What is your name?</span>
+    </label>
+    <input className="input input-bordered w-full" onClick={onClick} ref={ref} value={value} />
+  </div>
+)
+);
+
 const CreateEventModal: React.FC<Props> = ({
-    disclosure: { onClose, isOpen },
-    currentDate,
+  currentDate,
+  handleClick
 }) => {
-    const queryClient = useQueryClient();
-    const selectedGuild = useSelectedGuild(state => state.selectedGuild);
+  const [date, setDate] = useState<Date | null>(currentDate)
+  const queryClient = useQueryClient();
+  const selectedGuild = useSelectedGuild(state => state.selectedGuild);
 
-    const mutation = useMutation(createEvent, {
-        onMutate: (data: Omit<EventAttributes, "id">) => {
-            queryClient.cancelQueries("events");
+  const mutation = useMutation(createEvent, {
+    onMutate: (data: Omit<EventAttributes, "id">) => {
+      queryClient.cancelQueries("events");
 
-            const snapshot =
-                queryClient.getQueryData<Response<EventAttributes[]>>("events");
+      const snapshot =
+        queryClient.getQueryData<Response<EventAttributes[]>>("events");
 
-            snapshot &&
-                queryClient.setQueryData<Response<EventAttributes[]>>(
-                    "events",
-                    (prev) => ({
-                        data: [
-                            ...snapshot.data,
-                            {
-                                id: Math.random(),
-                                ...data,
-                            },
-                        ],
-                        message: prev!.message,
-                        success: prev!.success,
-                    })
-                );
+      snapshot &&
+        queryClient.setQueryData<Response<EventAttributes[]>>(
+          "events",
+          (prev) => ({
+            data: [
+              ...snapshot.data,
+              {
+                id: Math.random(),
+                ...data,
+              },
+            ],
+            message: prev!.message,
+            success: prev!.success,
+          })
+        );
 
-            return { snapshot };
-        },
-        onError: (_, __, context) => {
-            if (context?.snapshot) {
-                queryClient.setQueryData<Response<EventAttributes[]>>(
-                    "events",
-                    context.snapshot
-                );
-            }
-        },
-        onSettled: () => queryClient.invalidateQueries("events"),
-    });
-    return (
-        <>
-            <Modal isOpen={isOpen!} onClose={onClose!} isCentered>
-                <ModalOverlay />
-                <ModalContent bg="var(--background-secondary)">
-                    <ModalHeader color="white">Create new Event</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody paddingBottom="24px">
-                        <Formik
-                            initialValues={{
-                                title: "",
-                                start: currentDate?.toString() || "",
-                                end: "",
-                            }}
-                            onSubmit={(values, { setSubmitting }) => {
-                                setSubmitting(true);
-                                mutation.mutate({
-                                    title: values.title,
-                                    start: currentDate,
-                                    end: moment(values.end).toDate(),
-                                    guildId: selectedGuild!.id,
-                                });
+      return { snapshot };
+    },
+    onError: (_, __, context) => {
+      if (context?.snapshot) {
+        queryClient.setQueryData<Response<EventAttributes[]>>(
+          "events",
+          context.snapshot
+        );
+      }
+    },
+    onSettled: () => queryClient.invalidateQueries("events"),
+  });
 
-                                onClose && onClose();
-                                setSubmitting(false);
-                            }}
-                        >
-                            {({ isSubmitting, errors, touched, setFieldValue, values }) => (
-                                <Form>
-                                    <VStack spacing={5}>
-                                        <FormInput
-                                            isInvalid={!!errors.title && touched.title}
-                                            name="title"
-                                            type="text"
-                                            placeholder="title"
-                                            errorMessage={errors.title}
-                                            label="Title"
-                                        />
-
-                                        <FormControl>
-                                            <FormLabel color="white">Start date</FormLabel>
-                                            <DatePickerField
-                                                name="start"
-                                                onChangeFunc={setFieldValue}
-                                                value={values.start}
-                                            />
-                                        </FormControl>
-
-                                        <FormControl>
-                                            <FormLabel color="white">End date</FormLabel>
-                                            <DatePickerField
-                                                name="end"
-                                                onChangeFunc={setFieldValue}
-                                                value={values.end}
-                                            />
-                                        </FormControl>
-                                        <Button
-                                            w="100%"
-                                            mt={4}
-                                            bg="var(--background-secondary-alt)"
-                                            isLoading={isSubmitting}
-                                            type="submit"
-                                            color="var(--text-primary)"
-                                        >
-                                            Submit
-										</Button>
-                                    </VStack>
-                                </Form>
-                            )}
-                        </Formik>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <input type="checkbox" id="createEventModal" className="modal-toggle" />
+      <div className="modal modal-open">
+        <div className="modal-box relative overflow-y-visible">
+          <label htmlFor="createEventModal" className="btn btn-sm btn-circle absolute right-2 top-2" onClick={handleClick}>✕</label>
+          <h3 className="text-lg font-bold">Crea un evento en el calendario</h3>
+          <DatePicker
+            dateFormat="dd/MM/yyyy HH:mm"
+            timeFormat="HH:mm"
+            showTimeSelect
+            locale="es"
+            selected={date}
+            onChange={(date: Date | null) => setDate(date)}
+            customInput={<CustomInput />}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 type DatePickerFieldProps = {
-    name: string;
-    value: string;
-    onChangeFunc: (
-        field: string,
-        value: any,
-        shouldValidate?: boolean | undefined
-    ) => void;
+  name: string;
+  value: string;
+  onChangeFunc: (
+    field: string,
+    value: any,
+    shouldValidate?: boolean | undefined
+  ) => void;
 };
 const DatePickerField: React.FC<DatePickerFieldProps> = ({
-    name,
-    value,
-    onChangeFunc,
+  name,
+  value,
+  onChangeFunc,
 }) => {
-    return (
-        <DatePicker
-            selected={(value && new Date(value)) || null}
-            onChange={(val) => {
-                onChangeFunc(name, val);
-            }}
-        />
-    );
+  return (
+    <DatePicker
+      selected={(value && new Date(value)) || null}
+      onChange={(val) => {
+        onChangeFunc(name, val);
+      }}
+    />
+  );
 };
 
 export default CreateEventModal;
